f0631d054f8b7615dab903c21d64caf310ae342b
Pamp47 <xyliga6@rambler.ru>
2022-04-08 16:23:52 +0300

    Initial commit

---
+++ lesson1/task1.py
@@ -0,0 +1,56 @@
+"""
+Реализовать вывод информации о промежутке времени в зависимости от его
+продолжительности duration в секундах:
+a. до минуты: <s> сек;
+b. до часа: <m> мин <s> сек;
+c. до суток: <h> час <m> мин <s> сек;
+d. * в остальных случаях: <d> дн <h> час <m> мин <s> сек.
+"""
+
+
+# duration = int(input('Enter the time in seconds: '))
+# day = duration // 60 // 60 // 24
+# hour = duration // 60 // 60 % 24
+# minutes = duration // 60 % 60
+# seconds = duration % 60
+# print(f'Дней: {day}, часов: {hour}, минут: {minutes}, секунд: {seconds}')
+
+
+def naive_realisation(duration: int):
+    # total_time = str(f'Дней: {day}, часов: {hour}, минут: {minutes}, секунд: {seconds}')
+    """
+    Решение задачи БЕЗ использования циклов.
+    Переменная total_time - строковая переменная,
+    содержащая в себе промежуток времени в нужно формате
+    """
+    day = duration // 60 // 60 // 24
+    hour = duration // 60 // 60 % 24
+    minutes = duration // 60 % 60
+    seconds = duration % 60
+
+    total_time = str(f'{day} дн {hour} час {minutes} мин {seconds} сек')
+    # print(type(total_time))
+    return total_time
+
+
+def one_cycle_realisation(duration):
+    # total_time = str(f'Дней: {day}, часов: {hour}, минут: {minutes}, секунд: {seconds}')
+    """
+    Решение задачи с использования циклов.
+    Можно два, но высший пилотаж через 1 цикл.
+    Переменная total_time - строковая переменная,
+    содержащая в себе промежуток времени в нужно формате
+    """
+    while duration > 0:
+        day = duration // 60 // 60 // 24
+        hour = duration // 60 // 60 % 24
+        minutes = duration // 60 % 60
+        seconds = duration % 60
+        total_time = str(f'{day} дн {hour} час {minutes} мин {seconds} сек')
+        return total_time
+
+
+if __name__ == '__main__':
+    duration = 123532
+    print(naive_realisation(duration))
+    print(one_cycle_realisation(duration))
---
+++ lesson1/task3.py
@@ -0,0 +1,35 @@
+"""
+Склонение слова
+Реализовать склонение слова «процент» во фразе «N процентов».
+Вывести эту фразу на экран отдельной строкой для каждого из чисел в интервале от 1 до 100:
+1 процент
+2 процента
+3 процента
+4 процента
+5 процентов
+6 процентов
+...
+100 процентов
+"""
+
+percent = 1
+while percent <= 100:
+    if 10 <= percent <= 20:
+        print(percent, 'процентов')
+    elif percent == 1 or percent % 10 == 1:
+        print(percent, 'процент')
+    elif percent % 10 == 5 or percent % 10 == 6 or percent % 10 == 7 or percent % 10 == 8 or percent % 10 == 9 or percent % 10 == 0:
+        print(percent, 'процентов')
+    elif percent % 10 == 2 or percent % 10 == 3 or percent % 10 == 4:
+        print(percent, 'процента')
+    percent += 1
+
+
+def transform_string(number: int) -> str:
+    """Возвращает строку вида 'N процентов' с учётом склонения по указанному number"""
+
+    # return 'верните отформатированную строку'
+
+# if __name__ == '__main__':
+#     for n in range(1, 101):  # по заданию учитываем только значения от 1 до 100
+#         print(transform_string(n))
